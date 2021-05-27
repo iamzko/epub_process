892e8f08be853d3aa0302c5b300d4199724e86ce
iamzko <iamzko@126.com>
2021-05-27 18:06:07 +0800

    1.创建工程 2.添加第三方库 3.测试minizip解压EPUB文件流程

---
+++ src/epub_process_zh_CN.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>S:</name>
+    <message>
+        <source></source>
+        <comment>the capital letters are shortcuts, mark one letter similarly in your translation and translate the standalone letter accordingly later</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source></source>
+        <comment>translate this letter according to the shortcut letter used previously in the translation of &quot;R)epl&quot; before</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source></source>
+        <comment>translate these letters according to the shortcut letter used previously in the translation of &quot;U)ncap&quot; and I)nsert before</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source></source>
+        <comment>translate this letter according to the shortcut letter used previously in the translation of &quot;U)ncap&quot; and I)nsert before</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source></source>
+        <comment>translate this letter according to the shortcut letter used previously in the translation of &quot;S)tem&quot; before</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source></source>
+        <comment>translate this letter according to the shortcut letter used previously in the translation of &quot;e(X)it&quot; before</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source></source>
+        <comment>translate this letter according to the shortcut letter used previously in the translation of &quot;Q)uit&quot; before</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source></source>
+        <comment>translate this letter according to the shortcut letter y)es</comment>
+        <translation></translation>
+    </message>
+</context>
+</TS>
